edec3e6b5db715e0aa651705862b5d5ebca30955
Heidi Rytkonen <heimarry@puck.vrl.jyu.fi>
2020-09-14 11:40:47 +0300

    small fix

--- submit.sh
+++ submit.sh
@@ -12,7 +12,7 @@ then
     exit 0
 fi
 
-inputfiles=(input/cent00*/*)
+inputfiles=(input/cent40*/*)
 if [ -z "$2" ]
 then
     njobs=${#inputfiles[@]}
@@ -23,12 +23,13 @@ fi
 for (( i=0; i<$njobs; i++ ))
 do
     n=$((i+1))
-    nevents=$( echo ${inputfiles[$i]#*_n} | grep -o -E '[0-9]+' )
+    #nevents=$( echo ${inputfiles[$i]#*_n} | grep -o -E '[0-9]+' )
+    nevents=1
     outputdir=outputs/run_${1}_job$n
     mkdir $outputdir
     mkdir ${outputdir}/logs
-    #idA=$(sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J simO2 --time=02:00:00 --ntasks 1 --cpus-per-task 12 run $outputdir ${inputfiles[$i]} $nevents | awk '{print $4}')
-    #sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J simO2 --dependency=afterok:1297364 --ntasks 1 --cpus-per-task 12 run $outputdir ${inputfiles[$i]} $nevents
-    sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J simO2 --ntasks 1 --cpus-per-task 12 run $outputdir ${inputfiles[$i]} $nevents
+    #idA=$(sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J simO2 --time=02:00:00 -n 12 --exclusive run $outputdir ${inputfiles[$i]} $nevents | awk '{print $4}')
+    #sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J simO2 --dependency=afterok:1297364 -n 12 --exclusive run $outputdir ${inputfiles[$i]} $nevents
+    sbatch -o ${outputdir}/logs/log -e ${outputdir}/logs/errout -J bgtest -n 12 --exclusive run $outputdir ${inputfiles[$i]} $nevents
     sleep 1
 done
